261dd06cc1d8d79aea09ecca95c89a1b532e8ecf
Kate Dameron <katerj33@gmail.com>
2019-01-11 10:00:18 -0800

    importing constants and finished test

--- test/bitmap-header.test.js
+++ test/bitmap-header.test.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const constants = require('../lib/bitmap-constants');
 const BitmapHeader = require('../lib/bitmap-header');
 
@@ -5,8 +6,10 @@ describe('bitmap header', () => {
 
     let buffer = null;
     beforeEach('read the file', done => {
-        fs.readFile('./test/test-bitmap.bmp', { encoding: 'utf8' }, (err, callback) => {
-            buffer
+        fs.readFile('./test/test-bitmap.bmp', (err, data) => {
+            expect(err).toBeFalsy;
+            buffer = data;
+            done();
         })
         // TODO: file read './test/test-bitmap.bmp' and put the promise return into buffer variable
     });
@@ -25,8 +28,8 @@ describe('bitmap header', () => {
         // on the BitmapHeader in its constructor.
         // These test values are correct for the supplied test-bitmap.bmp
         const header = new BitmapHeader(buffer);
-        expect(header.pixelOffset, 54).toEqual();
-        expect(header.bitsPerPixel, 24).toEqual();
-        expect(header.fileSize, 30054).toEqual();
+        expect(header.pixelOffset).toEqual(54);
+        expect(header.bitsPerPixel).toEqual(24);
+        expect(header.fileSize).toEqual(30054);
     });
 });
