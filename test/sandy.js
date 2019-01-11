14b8e09948354d35bea45eba0168dc406866fb78
Shaba9 <shaba92@gmail.com>
2019-01-11 10:05:48 -0800

    created bitmap header class and uint stuff

--- test/sandy.js
+++ test/sandy.js
@@ -1,6 +1,15 @@
 const fs = require('fs');
 const Buffer = require('buffer').Buffer;
+let buffer = null;
 
 fs.readFile('./test-bitmap.bmp', { encoding: 'utf8' }, (err, data) => {
-  console.log(Buffer.from(data));
+  buffer = Buffer.from(data);
+  console.log(buffer[10]);
+  console.log(buffer[28]);
+  console.log(buffer[2]);
+  console.log('10', buffer.readUInt32LE(10));
+  console.log('2', buffer.readUInt16LE(2));
+  console.log('28', buffer.readUInt16LE(28));
+
 });
+
