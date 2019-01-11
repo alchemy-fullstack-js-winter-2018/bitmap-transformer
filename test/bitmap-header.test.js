450c5f4cc654384f087d09838dcc52e159777909
Marcy Silverman <marcysilve225@gmail.com>
2019-01-11 10:34:38 -0800

    header test passes

--- test/bitmap-header.test.js
+++ test/bitmap-header.test.js
@@ -2,36 +2,21 @@ const fs = require('fs');
 const constants = require('../lib/bitmap-constant');
 const BitmapHeader = require('../lib/bitmap-header');
 
-describe('bitmap header', done => {
+describe('bitmap header', () => {
   let buffer = null;
   beforeEach(done => {
     fs.readFile('./test/test-bitmap.bmp', (err, data) => {
       buffer = data;
       done();
+      expect(constants.PIXEL_OFFSET).toBeDefined();
+      expect(constants.BITS_PER_PIXEL_OFFSET).toBeDefined();
+      expect(constants.FILE_SIZE_OFFSET).toBeDefined();
     });
   });
-  // TODO: file read './test/test-bitmap.bmp' and put the promise return into buffer variable
-  it('reads the file', done => {
-    
+  it('parse header data', () => {
+    const header = new BitmapHeader(buffer); 
+    expect(header.pixelOffset).toEqual(54);
+    expect(header.bitsPerPixel).toEqual(24);
+    expect(header.fileSize).toEqual(30054); 
   });
-  
-
-  //   it('has correct specs', () => {
-  //     // TODO: read the wiki spec docs to figure out what these values should be.
-  //     // You don't need to change this test, you need to put the correct values into
-  //     // '../lib/bitmap-constants'
-  //     expect(constants.PIXEL_OFFSET).toBeDefined;
-  //     expect(constants.BITS_PER_PIXEL_OFFSET).toBeDefined;
-  //     expect(constants.FILE_SIZE_OFFSET).toBeDefined;
-  //   });
-
-//   it('parses header data', () => {
-//     // TODO: use the constants to populate the following properties
-//     // on the BitmapHeader in its constructor.
-//     // These test values are correct for the supplied test-bitmap.bmp
-//     const header = new BitmapHeader(buffer);
-//     expect(header.pixelOffset, 54).toEqual;
-//     expect(header.bitsPerPixel, 24).toEqual;
-//     expect(header.fileSize, 30054).toEqual;
-//   });
 });
