236d2b829d4c59ea519307e366da7795690dca28
Cari Palazzolo <cari.pizza@gmail.com>
2019-01-11 13:20:21 -0800

    update pixel-reader and test, passes

--- test/pixel-reader.test.js
+++ test/pixel-reader.test.js
@@ -4,41 +4,9 @@ describe('Pixel Reader', () => {
 
   it('reads pixel from buffer', done => {
     const reader = new PixelReader({ bitsPerPixel: 24 });
-        
+
     const colors = [];
 
-    // [0, 0, 0, 255, 255, 255, 255, 0, 0]
-
-    // {
-    //   offset: 0,
-    //   b: 0,
-    //   g: 0,
-    //   r: 0
-    // }
-
-    // {
-    //   offset: 3,
-    //   b: 255,
-    //   g: 255,
-    //   r: 255
-    // }
-
-    // {
-    //   offset: 6,
-    //   b: 255,
-    //   g: 0,
-    //   r: 0
-    // }
-
-
-    // TODO: subscribe to reader "color" event and push into `colors` array.
-    // A "color" object should look like:
-    // {
-    //     offset: <offset from the start of buffer passed to PixelReader>,
-    //     r: <red color value>,
-    //     g: <green color value>,
-    //     b: <blue color value>,
-    // }
     reader.on('color', color => {
       colors.push(color);
     });
@@ -48,8 +16,8 @@ describe('Pixel Reader', () => {
       expect(colors[0]).toEqual({
         offset: 0,
         r: 0,
-        b: 0,
-        g: 0
+        g: 0,
+        b: 0
       });
 
       expect(colors[1]).toEqual({
@@ -68,25 +36,25 @@ describe('Pixel Reader', () => {
       done();
     });
 
-    // Create a buffer with known data for your colors
     const buffer = Buffer.alloc(9); // for three pixels
-    // TODO: fill buffer with byte values that match your 
-    // expected test colors
 
-    // first color black
+    // first color is black
+    // #000000
     buffer.writeUInt8(0);
-    buffer.writeUInt8(0);
-    buffer.writeUInt8(0);
+    buffer.writeUInt8(0, 1);
+    buffer.writeUInt8(0, 2);
 
     // second color white
-    buffer.writeUInt8(255);
-    buffer.writeUInt8(255);
-    buffer.writeUInt8(255);
+    // #FFFFFF
+    buffer.writeUInt8(255, 3);
+    buffer.writeUInt8(255, 4);
+    buffer.writeUInt8(255, 5);
 
-    // third color blue b,g,r
-    buffer.writeUInt8(255);
-    buffer.writeUInt8(0);
-    buffer.writeUInt8(0);
+    // third color blue
+    // #FF0000
+    buffer.writeUInt8(255, 6);
+    buffer.writeUInt8(0, 7);
+    buffer.writeUInt8(0, 8);
 
     // Call read method with your buffer
     reader.read(buffer);
