7c8714ce4afbcf7e127e51dfdbc616238354d907
Lance Owen Merrill <Lanceom@hawaii.edu>
2019-01-11 09:22:15 -0800

    found some values in const file

--- test/bitmap-header.test.js
+++ test/bitmap-header.test.js
@@ -1,4 +1,3 @@
-const assert = require('assert');
 const constants = require('../lib/bitmap-constants');
 const BitmapHeader = require('../lib/bitmap-header');
 
@@ -6,25 +5,25 @@ describe('bitmap header', () => {
 
     let buffer = null;
     beforeEach(() => {
-        // TODO: file read './test/test-bitmap.bmp' and put the promise return into buffer variable
+    // TODO: file read './test/test-bitmap.bmp' and put the promise return into buffer variable
     });
 
     it('has correct specs', () => {
-        // TODO: read the wiki spec docs to figure out what these values should be.
-        // You don't need to change this test, you need to put the correct values into
-        // '../lib/bitmap-constants'
-        assert.ok(constants.PIXEL_OFFSET);
-        assert.ok(constants.BITS_PER_PIXEL_OFFSET);
-        assert.ok(constants.FILE_SIZE_OFFSET);
+    // TODO: read the wiki spec docs to figure out what these values should be.
+    // You don't need to change this test, you need to put the correct values into
+    // '../lib/bitmap-constants'
+        expect(constants.PIXEL_OFFSET).toBeUndefined();
+        expect(constants.BITS_PER_PIXEL_OFFSET).toBeUndefined();
+        expect(constants.FILE_SIZE_OFFSET).toBeUndefined();
     });
 
     it('parses header data', () => {
-        // TODO: use the constants to populate the following properties
-        // on the BitmapHeader in its constructor.
-        // These test values are correct for the supplied test-bitmap.bmp
+    // TODO: use the constants to populate the following properties
+    // on the BitmapHeader in its constructor.
+    // These test values are correct for the supplied test-bitmap.bmp
         const header = new BitmapHeader(buffer);
-        assert.equal(header.pixelOffset, 54);
-        assert.equal(header.bitsPerPixel, 24);
-        assert.equal(header.fileSize, 30054);
+        expect(header.pixelOffset, 54);
+        expect(header.bitsPerPixel, 24);
+        expect(header.fileSize, 30054);
     });
 });
