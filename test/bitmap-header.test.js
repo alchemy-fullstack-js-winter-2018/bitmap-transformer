1010fd6f95ad95bc7d5601b3b5e32b4892223ee5
Masmics <cherlocker3113@gmail.com>
2019-01-11 10:06:12 -0800

    bitmap-header IP

--- test/bitmap-header.test.js
+++ test/bitmap-header.test.js
@@ -1,11 +1,15 @@
+const fs = require('fs');
 const constants = require('../lib/bitmap-constants');
 const BitmapHeader = require('../lib/bitmap-header');
 
 describe('bitmap header', () => {
 
   let buffer = null;
-  beforeEach(() => {
-    // TODO: file read './test/test-bitmap.bmp' and put the promise return into buffer variable
+  beforeEach(done => {
+    fs.readFile('./test-bitmap.bmp', (err, data => {
+      buffer = data;
+      done();
+    }));
   });
 
   it('has correct specs', () => {
