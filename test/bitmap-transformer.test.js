65a1cbae6f5a560add5f617d2bba4643209031f8
Ivan <i.poblete915@gmail.com>
2019-01-11 14:39:36 -0800

    all test passing

--- test/bitmap-transformer.test.js
+++ test/bitmap-transformer.test.js
@@ -8,25 +8,16 @@ describe('bitmap file transformer', () => {
   let buffer = null;
   beforeEach(() => {
     buffer = readFileSync('./test/test-bitmap.bmp');
-    
   });
 
-  it('test whole transform', done => {
-   
+  it('will test whole transform', done => {
     const bitmap = new BitmapTransformer(buffer);
-
     bitmap.transform(invert, err => {
       if(err) return done(err);
 
       const expected = readFileSync('./test/inverted-expected.bmp');
       assert.deepEqual(bitmap.buffer, expected);
       done();
-
-      // If you don't have a standard file yet, or need to update or are adding new test,
-      // you can write it out by commenting above code block, and uncomment code below 
-      // that writes the file and then visually inspect the file for correctness.
-      // return fs.writeFileSync('./test/inverted-expected.bmp', bitmap.buffer);
     });
-
   });
 });
--- test/tranformers.test.js
+++ test/tranformers.test.js
@@ -1,18 +1,17 @@
-const assert = require('assert');
 const invert = require('../lib/invert-transformer');
 const grayscale = require('../lib/grayscale-transformer');
+const cool = require('../lib/cool-transformer');
 
 describe('transformers', () => {
 
   it('invert', () => {
-    // HINT: invert subtracts each value from 255
     const transformed = invert({
       r: 34,
       g: 100,
       b: 205
     });
 
-    assert.deepEqual(transformed, {
+    expect(transformed).toEqual({
       r: 221,
       g: 155,
       b: 50
@@ -20,20 +19,30 @@ describe('transformers', () => {
   });
 
   it('grayscale', () => {
-    // HINT: grayscale assigns the average of all three colors
-    // as the new value for each color
     const transformed = grayscale({
       r: 34,
       g: 100,
       b: 205
     });
 
-    assert.deepEqual(transformed, {
+    expect(transformed).toEqual({
       r: 113,
       g: 113,
       b: 113
     });
   });
 
-  // TODO: add a third transformer (you'll need to add the module and require!) and test
+  it('will cool the image', () => {
+    const transformed = cool({
+      r: 34, 
+      g: 100, 
+      b: 205
+    });
+
+    expect(transformed).toEqual({
+      r: 34,
+      g: 100, 
+      b: 255
+    });
+  });
 });
